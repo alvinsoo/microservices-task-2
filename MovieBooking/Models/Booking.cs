97c342dc5c21a6eb0d4006cb2d916b8514fbe9e3
Alvin Soo <alvin_soo@simtech.a-star.edu.sg>
2021-09-10 15:44:14 +0800

    Task 2

---
+++ MovieBooking/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBooking.Models
+{
+    public class Booking
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        public int NumberOfTickets { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+        
+        [Required]
+        public string Currency { get; set; }
+
+    }
+}
--- MovieBooking/Startup.cs
+++ MovieBooking/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MovieBooking.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,10 @@ namespace MovieBooking
         {
 
             services.AddControllers();
+
+            services.AddDbContext<BookingContext>(opt =>
+                                               opt.UseInMemoryDatabase("BookingList"));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieBooking", Version = "v1" });
